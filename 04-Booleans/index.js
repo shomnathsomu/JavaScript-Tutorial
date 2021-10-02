7e4671c66ce1d2106a6e5750a5c2f3e5a2687e8a
Shomnath Pramanik <shomnathcse22@gmail.com>
2021-10-02 20:01:34 +0600

    Created boolean operators

---
+++ 04-Booleans/index.js
@@ -0,0 +1,19 @@
+var num1 = 10;
+var num2 = 20;
+
+console.log(num1>num2);
+console.log(num1<num2);
+
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 === "5");
+
+
+console.log(Boolean("Hello World!"));
+console.log(Boolean(""));
+console.log(Boolean(num1>num2));
+console.log(Boolean(num2>num1));
+
+console.log(Boolean(0));
+console.log(Boolean(-5));
+console.log(Boolean(7));
