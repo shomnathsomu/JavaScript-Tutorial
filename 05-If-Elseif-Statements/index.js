59d8044d5b012be825d63d11cae9a39705e0b29a
Shomnath Pramanik <shomnathcse22@gmail.com>
2021-10-02 22:43:19 +0600

    Added If Else statements with example

---
+++ 05-If-Elseif-Statements/index.js
@@ -0,0 +1,10 @@
+var num1 = 10;
+var num2 = 10;
+
+if (num1 > num2) {
+    document.write("Here we go!");
+} else if (num1 < num2) {
+    document.write("We won't go!");
+} else {
+    document.write("They are neutral");
+}
