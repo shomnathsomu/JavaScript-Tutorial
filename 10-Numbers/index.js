dc42ba417a1ff05b76bd553f974e3ab6cdeb8970
Shomnath Pramanik <shomnathcse22@gmail.com>
2021-10-05 18:07:55 +0600

    Added Math functions for numbers

---
+++ 10-Numbers/index.js
@@ -0,0 +1,20 @@
+var num1 = 7;
+var num2 = "apple";
+
+var sum = num2 + num1;
+
+console.log(sum);
+console.log(typeof(sum));
+
+console.log(Math.round(10.6));
+console.log(Math.round(10.4));
+console.log(Math.ceil(10.4));
+console.log(Math.floor(10.4));
+console.log(Math.max(10.4, 28.3, 99.9, 87.5));
+console.log(Math.PI);
+
+if(isNaN(num1*num2)){
+    console.log("The answer is not a number!");
+} else {
+    console.log("The answer is really cool!");
+}
